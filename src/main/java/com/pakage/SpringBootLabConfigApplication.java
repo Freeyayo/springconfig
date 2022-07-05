ace5c34baee290221d57cbdbaf13cb9378b07a27
Kevin Cai <kevin_cai@epam.com>
2022-07-05 11:55:49 +0800

    init

---
+++ src/main/java/com/pakage/SpringBootLabConfigApplication.java
@@ -0,0 +1,13 @@
+package com.pakage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLabConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pakage/SpringBootLabConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pakage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLabConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
